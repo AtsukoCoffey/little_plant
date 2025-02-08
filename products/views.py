68fb4c704fc6f33882a0d5326ba37c2a82ccfae3
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-08 22:49:22 +0000

    feat: Create product detail page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import PlantItem
 
 
@@ -11,3 +11,19 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, slug):
+    """ 
+    A view to show individual product details
+    Display an individual product :model:`products.PlantItem`.
+
+    **Context**
+    
+    """
+    product = get_object_or_404(PlantItem, slug=slug)
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
