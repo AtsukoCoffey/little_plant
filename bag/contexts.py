ab721a24929e3d3e401bdd5ed78ee2dd19f7df39
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-09 14:50:50 +0000

    feat: Update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,12 +1,30 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import PlantItem
 
 
 def bag_contents(request):
+    """
+    The 'bag' contents stored by 'add_to_bag' into the session
+    This function can be accessed from any pages to get the contents
+    : registered 'context_processors'
+    """
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(PlantItem, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
