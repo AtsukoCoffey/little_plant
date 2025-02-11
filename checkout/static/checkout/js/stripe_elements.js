2ed5fd1a43841793b0b92df4dfab4367122a276f
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-11 10:50:32 +0000

    Checkout - Add loading spinner overlay to stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -70,6 +70,8 @@ form.addEventListener('submit', function(ev) {
     card.update({ 'disabled': true});  
 
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     // Using python variables in JavaScript 
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
@@ -85,6 +87,8 @@ form.addEventListener('submit', function(ev) {
                 <span>${result.error.message}</span>
             `;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             // We'll want to re-enable the card element and the submit button to allow the user to fix it.
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
