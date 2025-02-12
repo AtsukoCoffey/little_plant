13974aba0d4bba106aaccb7f9d52ac91e3f766e1
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-12 00:05:10 +0000

    feat: Checkout - Add form data cashing in payment intent: Webhook

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -39,10 +39,10 @@ var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 
-// Handle realtime validation errors on the card element
-// change event listener: checks errors everytime it changes
-card.addEventListener('change', function (event) {
 
+/**  Handle realtime validation errors on the card element
+    change event listener: checks errors everytime it changes   */
+card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
 
     if (event.error) {
@@ -59,43 +59,85 @@ card.addEventListener('change', function (event) {
 });
 
 
-// Handle form submit 
+
+/**  Handle form submit
+ *   saveInfo value is in 'postData' Other order information is in
+ *   'stripe.confirmCardPayment()' and send to Stripe and  */
 var form = document.getElementById('payment-form');
 
-// function - 'stripe.confirmCardPayment()' in here
 form.addEventListener('submit', function(ev) {
-
     ev.preventDefault();  // Default action is POST
     // card button and submission to prevent multiple submissions
     card.update({ 'disabled': true});  
-
+    // Stop submitting
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    // Using python variables in JavaScript 
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>
-            `;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            // We'll want to re-enable the card element and the submit button to allow the user to fix it.
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+
+        // Using python variables in JavaScript 
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                // data redundancy - add order info from here
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
             }
-        }
-    });
+
+        }).then(function(result) {
+            // Error handling
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>
+                `;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                // We'll want to re-enable the card element and the submit button to allow the user to fix it.
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
+    });       
 });
