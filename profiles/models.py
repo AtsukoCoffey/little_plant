9a312721826eb12ea8cfe5192e956689ebf5481d
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-13 00:16:16 +0000

    style: Update Allauth templates

--- profiles/models.py
+++ profiles/models.py
@@ -28,6 +28,8 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
+    First time force to create a profile and runserver then login
+    UserProfile.objects.create(user=instance)
     """
     if created:
         UserProfile.objects.create(user=instance)
