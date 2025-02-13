8b363258111fa2c5e8bc67825c01d41747d5df1e
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-13 14:57:54 +0000

    feat: Profile - save info from webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import PlantItem
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -52,6 +53,21 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         # Determine the order already exists or not. Using 'attempt'
         order_exists = False
         attempt = 1
@@ -86,6 +102,7 @@ class StripeWH_Handler:
             try:  # objects.create because there wasn't made yet
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
