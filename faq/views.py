306be8c68c6bc443693081d0fde373e49714fa36
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-19 01:23:39 +0000

    feat: Faq - Filter the data to display categorized pages

--- faq/views.py
+++ faq/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
-from .models import Faq
+from .models import Faq, FaqCategory
 
 
 def faq(request):
 
+    faqs = Faq.objects.all()
+
+    if request.GET:
+        category = request.GET['category']
+        faqs = Faq.objects.filter(category=category)
+        category = FaqCategory.objects.filter(pk=category)
+
     context = {
-        'faqs': Faq.objects.all(),
+        'faqs': faqs,
+        'category': category[0],
     }
 
     return render(request, 'faq/faq.html', context)
