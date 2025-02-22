9f78b4c8c9b70760614502b563780a3a9c00341d
Atsuko Coffey <atsucoffey@gmail.com>
2025-02-22 15:07:09 +0000

    test: Email smtp is not working - change settings

--- config/settings.py
+++ config/settings.py
@@ -214,9 +214,9 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Sending email settings
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = os.environ.get('email@backends.console')
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -224,6 +224,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Delivery info for chaeckout calculation
 FREE_DELIVERY_THRESHOLD = 50
